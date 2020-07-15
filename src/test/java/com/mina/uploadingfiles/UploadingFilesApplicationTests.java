07926c3208d0853eb42404e35893862225bda925
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-15 16:26:46 +0200

    initial commit

---
+++ src/test/java/com/mina/uploadingfiles/UploadingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mina.uploadingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadingFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
